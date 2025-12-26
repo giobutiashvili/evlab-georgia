6ea566c7e56dbf4c785719ec8bf50b6bf2273f85
giorgi.b <100768279+giobutiashvili@users.noreply.github.com>
2025-12-27 00:14:39 +0400

    fix route

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,16 +2,16 @@ import { createRouter, createWebHistory } from "vue-router";
 
 // Pages
 import FrontLayout from "@/Layouts/FrontLayout.vue";
-import Home from "../Pages/Home.vue";
-import Products from "../Pages/Products.vue";
+import Home from "@/Pages/Home.vue";
+import Products from "@/Pages/Products.vue";
 import ProductDetails from "@/Pages/ProductDetails.vue";
-import AboutUs from "../Pages/AboutUs.vue";
-import Contact from "../Pages/Contact.vue";
+import AboutUs from "@/Pages/AboutUs.vue";
+import Contact from "@/Pages/Contact.vue";
 
 // Admin
 
 import AdminLayout from "@/Layouts/AdminLayout.vue";
-import AdminDashboard from "../AdminVue/AdminDashboard.vue";
+import AdminDashboard from "@/AdminVue/AdminDashboard.vue";
 import AdminProductAdd from "@/AdminVue/AdminProductAdd.vue";
 import AdminProductEdit from "@/AdminVue/AdminProductEdit.vue";
 import AdminLogin from "@/AdminVue/AdminLogin.vue";
@@ -62,7 +62,6 @@ const routes = [
         component: AdminLayout,
         meta: { requiresAdmin: true },
         children: [
-            { path: "", redirect: "/admin/login" }, // აქ კეთდება redirect
             {
                 path: "dashboard",
                 name: "AdminDashboard",
@@ -78,20 +77,16 @@ const routes = [
                 name: "AdminProductEdit",
                 component: AdminProductEdit,
             },
-            {
-                path: ":catchAll(.*)",
-                beforeEnter: (to, from, next) => {
-                    const token = localStorage.getItem("adminToken");
-                    if (token) next({ name: "AdminDashboard" });
-                    else next({ name: "AdminLogin" });
-                },
-            },
         ],
     },
+    { path: "/admin/login", name: "AdminLogin", component: AdminLogin },
     {
-        path: "/admin/login",
-        name: "AdminLogin",
-        component: AdminLogin,
+        path: "/admin/:catchAll(.*)",
+        beforeEnter: (to, from, next) => {
+            const token = localStorage.getItem("adminToken");
+            if (token) next({ name: "AdminDashboard" });
+            else next({ name: "AdminLogin" });
+        },
     },
 ];
 
