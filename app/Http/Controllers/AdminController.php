c5a8c506f8bfc4b660f168149cc72bcb541fcd1e
giorgi.b <100768279+giobutiashvili@users.noreply.github.com>
2025-12-26 23:37:54 +0400

    Fix admin login: correct Hash facade namespace

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\support\Facades\Hash;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
