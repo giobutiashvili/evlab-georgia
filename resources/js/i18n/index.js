22f90c3829e3d13f6d782c2c83750872c4ca4a46
giorgi.b <100768279+giobutiashvili@users.noreply.github.com>
2026-01-04 22:47:11 +0400

    update language

--- resources/js/i18n/index.js
+++ resources/js/i18n/index.js
@@ -2,13 +2,14 @@ import { createI18n } from "vue-i18n";
 import en from "./locales/en.json";
 import ka from "./locales/ka.json";
 
-export const i18n = createI18n({
+const i18n = createI18n({
     legacy: false,
-    locale: "ka",
+    locale: localStorage.getItem("lang") || "ka",
     fallbackLocale: "en",
     messages: {
         ka,
         en,
     },
-    locale: localStorage.getItem("lang") || "ka",
 });
+
+export default i18n;
